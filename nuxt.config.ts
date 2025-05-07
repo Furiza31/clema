8191d3d29898de727966a49ae0468cb2a515aaf1
Furiza31 <hugo.wendjaneh@gmail.com>
2025-05-07 19:57:03 +0000

    feat: integrate Tailwind CSS, DaisyUI and Lucide Icons, update project structure

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,22 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint"],
+  modules: [
+    "@nuxt/eslint",
+    "nuxt-lucide-icons",
+  ],
   eslint: {
     config: {
       standalone: false,
     },
+  },
+  css: ["~/assets/css/main.css"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  lucide: {
+    namePrefix: "Icon",
   },
 });
