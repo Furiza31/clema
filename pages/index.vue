652bbd7b5b8d26cccf5c7d8ba2309a5605f90855
Furiza31 <hugo.wendjaneh@gmail.com>
2025-05-07 20:35:39 +0000

    feat: restructure app layout and add navigation bar, update index page content

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hero bg-base-200 h-full w-full">
+    <div class="hero-content text-center">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">
+          Hello there
+        </h1>
+        <p class="py-6">
+          Simplify your life with Clema. Organize tasks, track habits, and achieve your goals with our intuitive life management solution. Start your journey to a more productive you today.
+        </p>
+        <button class="btn btn-primary">
+          Sign up
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
